6d65e3476c2e6055a4793fb17de5b86a446ab01b
Depanri Purba <depanri.purba@gmail.com>
2021-05-04 05:48:45 +0700

    Tambah data dan view data sudah selesai

--- lib/Tampildata.dart
+++ lib/Tampildata.dart
@@ -9,22 +9,48 @@ class Tampildata extends StatefulWidget {
 
 class _Tampildata extends State<Tampildata> {
   Widget build(BuildContext context) {
-    return GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount:
-                MediaQuery.of(context).orientation == Orientation.portrait
-                    ? 1
-                    : 1),
-                    itemCount: widget.data.length==null?0:widget.data.length,
-                    itemBuilder: (BuildContext context, int i){
-                      return new Card(
-                        child:Column(
-                          children: <Widget>[
-                            new Text(widget.data[i].nama),
-                            new Text(widget.data[i].jurusan)
-                          ],
-                        ) ,);
-                    },
-                    );
+    // return GridView.builder(
+    //     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+    //         crossAxisCount:
+    //             MediaQuery.of(context).orientation == Orientation.portrait
+    //                 ? 1
+    //                 : 1),
+    //                 itemCount: widget.data.length==null?0:widget.data.length,
+    //                 itemBuilder: (BuildContext context, int i){
+    //                   return new Card(
+    //                     child:Column(
+    //                       children: <Widget>[
+    //                         new Text(widget.data[i].nama),
+    //                         new Text(widget.data[i].jurusan)
+    //                       ],
+    //                     ) ,);
+    //                 },
+    //                 );
+    // return new ListView.builder(
+    //   itemCount: widget.data.length,
+    //   itemBuilder: (context,index){
+    //     return new Card(child: new ListTile(
+    //       leading: new Text(widget.data[index].nama),
+    //       title: new Text(widget.data[index].jurusan),
+    //     ),);
+    //   },
+    // );
+    return new DataTable(
+      columns: [
+        DataColumn(
+          label: new Text("nama")
+        ),
+        DataColumn(
+          label: new Text("jurusan")
+        ),
+      ],
+      rows: widget.data.map<DataRow>((element)=>DataRow(cells: [
+        DataCell(new Text(element.nama)),
+        DataCell(new Text(element.jurusan)),
+      ])).toList(),
+    );
+
+
+
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Aplikasi dengan sqflite",
       theme: ThemeData(
         primarySwatch: Colors.red,
@@ -76,6 +77,7 @@ class HalamanInputchild extends State<HalamanInput>{
     var mhs = Mahasiswa(cnama.text,cjurusan.text);
     await db.simpanData(mhs);
     print("data berhasil di simmpan");
+     return Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext contex)=> new HomePage()));
   }
   
   Widget build(BuildContext context){
